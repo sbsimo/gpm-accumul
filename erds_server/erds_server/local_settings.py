7ce6a8a0c4c2f451e87875c09edf1a0c82b2691e
sbsimo <simone.blb@gmail.com>
2018-02-21 09:30:07 +0100

    prepare local settings for production environmentt

---
+++ erds_server/erds_server/local_settings.py
@@ -0,0 +1,4 @@
+from erds_server.settings import *
+
+# add here all the local settings
+
--- erds_server/erds_server/wsgi.py
+++ erds_server/erds_server/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erds_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erds_server.local_settings")
 
 application = get_wsgi_application()
